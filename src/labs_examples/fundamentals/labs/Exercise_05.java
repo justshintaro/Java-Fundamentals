664f73f4d3f3939c1a4c50ee3eaf98d960e0d73c
justshintaro <snakamura01@manhattan.edu>
2021-10-14 14:34:07 -0400

    final submission for lab fundamental

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,38 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int intval = str.length();
+        System.out.println(intval);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean test = str.equals(str2);
+        System.out.println(test);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String new_str = str + str2;
+        System.out.println(new_str);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String str_upper = str.toUpperCase();
+        System.out.println(str_upper);
+
+        int search_1 = str2.indexOf("W");
+        int search_2 = str2.indexOf("o");
+        System.out.println(search_1 + search_2);
+
+        String str_sub = str.substring(2);
+        System.out.println(str_sub);
+
+//        String str_con = str.contains("hello");
+        System.out.println(str.contains("hello"));
+
+
+
+
+
+
 
     }
 
