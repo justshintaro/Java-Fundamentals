dc989a9ecf98ddc6ac7cd415a5274176d82c4805
justshintaro <snakamura01@manhattan.edu>
2021-10-14 14:06:25 -0400

    labs:fundamentals

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,29 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean isEmpty = false;
+        System.out.println(isEmpty);
+
+        char one_char = 'B';
+        System.out.println(one_char);
+
+        byte byteval = -128;
+        System.out.println(byteval);
+
+        short shortval = 32767;
+        System.out.println(shortval);
+
+        int intval = 1223232323;
+        System.out.println(intval);
+
+        long longval = -1223232323l;
+        System.out.println(longval);
+
+        float floatval = 2323.1223f;
+        System.out.println(floatval);
+
+        double doubleval = -123232.1223232;
+        System.out.println(doubleval);
 
     }
 
