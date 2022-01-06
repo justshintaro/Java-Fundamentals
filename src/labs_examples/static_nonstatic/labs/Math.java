212ee2463a3495d0c0135ad37e01caf9b9b1a17e
justshintaro <snakamura01@manhattan.edu>
2022-01-06 00:46:16 -0500

    complete static and non static

--- src/labs_examples/static_nonstatic/labs/Math.java
+++ src/labs_examples/static_nonstatic/labs/Math.java
@@ -1,5 +1,7 @@
 package labs_examples.static_nonstatic.labs;
 
+import org.omg.CORBA.ObjectHelper;
+
 import javax.print.attribute.standard.MediaSize;
 
 public class Math {
@@ -7,19 +9,44 @@ public class Math {
     public static void main(String[] args) {
 
         // 1) A static method calling another static method in the same class
+        System.out.println("implement 1)");
         additionTwo(1, 2);
 
         // 2) A static method calling a non-static method in the same class
+        System.out.println("\nimplement 2)");
         Math objA = new Math();
         objA.additionThree(1, 2 , 3);
 
         // 3) A static method calling a static method in another class
+        System.out.println("\nimplement 3)");
         OtherMath.Substraction(10, 2);
 
         // 4) A static method calling a non-static method in another class
+        System.out.println("\nimplement 4)");
         OtherMath objB = new OtherMath();
         objB.Division(5, 2);
 
+        // 5) A non-static method calling a non-static method in the same class
+        System.out.println("\nimplement 5)");
+        Math objC = new Math();
+        objC.callAdditionThree();
+
+        // 6) A non-static method calling a non-static method in another class
+        System.out.println("\nimplement 6)");
+        Math objE = new Math();
+        objE.callDivision();
+
+
+        // 7) A non-static method calling a static method in the same class
+        System.out.println("\nimplement 7)");
+        Math objF = new Math();
+        objF.callAdditionTwo();
+
+
+        // 8) A non-static method calling a static method in another class
+        System.out.println("\nimplement 8)");
+        Math objG = new Math();
+        objG.callSubtraction();
     }
 
     public static void additionTwo(int a, int b){
@@ -30,6 +57,24 @@ public class Math {
     public void additionThree(int a, int b, int c){
         int d = a + b + c;
         System.out.println(d);
+    }
+
+    public void callAdditionThree(){
+        additionThree(2, 3, 4);
+
+    }
+
+    public void callDivision(){
+        OtherMath objD = new OtherMath();
+        objD.Division(20, 4);
+    }
+
+    public void callAdditionTwo(){
+        additionTwo(100, 10);
+    }
+
+    public void callSubtraction(){
+        OtherMath.Substraction(100, 1);
     }
 
 }
--- src/labs_examples/static_nonstatic/labs/OtherMath.java
+++ src/labs_examples/static_nonstatic/labs/OtherMath.java
@@ -2,9 +2,6 @@ package labs_examples.static_nonstatic.labs;
 
 public class OtherMath {
 
-    public static void main(String[] args) {
-        Substraction(5, 2);
-    }
 
     public static void Substraction(int a, int b){
         int c = a -b;
@@ -12,7 +9,7 @@ public class OtherMath {
     }
 
     public void Division(float a, float b){
-        float c = a - b;
+        float c = a/b;
         System.out.println(c);
     }
 }
