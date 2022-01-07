d43753c9389e468fda939aa1c99329b20398c3fd
justshintaro <snakamura01@manhattan.edu>
2022-01-07 13:25:28 -0500

    add more functions to blackjack games

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -5,9 +5,10 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class BlackjackController {
 
-    public static void main(String[] args) {
 
-//        System.out.println(player2.getHand().over21());
+
+
+    public static void main(String[] args) {
 
         playBlackJack();
 
@@ -15,6 +16,11 @@ public class BlackjackController {
 
     public static void playBlackJack(){
 
+        int numOfGames = 0;
+        int numOfWins = 0;
+        int numOfLoss = 0;
+
+        // start a game
         Scanner scanner = new Scanner(System.in);
 
         // create two player objects
@@ -30,18 +36,19 @@ public class BlackjackController {
         // give the user and computer both an initial sum of money
         System.out.println("what is your sum of money, "+ username + "?");
         int budgetUser = scanner.nextInt();
-        Money moneyUser = new Money(budgetUser);
-        moneyUser.toString();
+        user.setPotValue(budgetUser);
 
         System.out.println("what is your sum of money, "+ computername + "?");
         int budgetPC = scanner.nextInt();
-        Money moneyPC = new Money(budgetPC);
-        moneyPC.toString();
+        computer.setPotValue(budgetPC);
 
-        while(moneyUser.budget > 0 && moneyPC.budget > 0){
+
+        while(user.getPotValue() > 0 && computer.getPotValue() > 0){
+
 
             // ask how much each player bets
-            System.out.println("\n"+username + " , how much would you like to bet?");
+            System.out.println();
+            System.out.println("\n"+username + " , how much would you like to bet? Currently, you have "+ user.getPotValue());
             int betUser = scanner.nextInt();
             scanner.nextLine();
             System.out.println(computername + " will bet five dollars");
@@ -99,119 +106,53 @@ public class BlackjackController {
 
             if(user.getHand().over21() && computer.getHand().over21()) {
                 System.out.println("Both you lost");
-                moneyUser.Betting(betUser, false);
-                moneyUser.toString();
-                moneyPC.Betting(betPC, false);
-                moneyPC.toString();
+                user.Betting(betUser, false);
+                computer.Betting(betPC, false);
 
             }else if(user.getHand().over21() && !computer.getHand().over21()){
                 System.out.println(computername + " win");
-                moneyUser.Betting(betUser, false);
-                moneyUser.toString();
-                moneyPC.Betting(betPC, true);
-                moneyPC.toString();
+                user.Betting(betUser, false);
+                computer.Betting(betPC, true);
+                numOfLoss++;
 
             }else if(!user.getHand().over21() && computer.getHand().over21()){
                 System.out.println(username + " win");
-                moneyUser.Betting(betUser, true);
-                moneyUser.toString();
-                moneyPC.Betting(betPC, false);
-                moneyPC.toString();
+                user.Betting(betUser, true);
+                computer.Betting(betPC, false);
+                numOfWins++;
 
             }else{
                 if(user.getHand().returnScore() < computer.getHand().returnScore()){
                     System.out.println(computername + " win");
-                    moneyUser.Betting(betUser, false);
-                    moneyUser.toString();
-                    moneyPC.Betting(betPC, true);
-                    moneyPC.toString();
+                    user.Betting(betUser, false);
+                    computer.Betting(betPC, true);
+                    numOfLoss++;
 
                 }else if(user.getHand().returnScore() > computer.getHand().returnScore()){
                     System.out.println(username + " win");
-                    moneyUser.Betting(betUser, true);
-                    moneyUser.toString();
-                    moneyPC.Betting(betPC, false);
-                    moneyPC.toString();
+                    user.Betting(betUser, true);
+                    computer.Betting(betPC, false);
+                    numOfWins++;
 
                 }else{
                     System.out.println("No winner, and try again");
-                    moneyUser.toString();
-                    moneyPC.toString();
-
                 }
             }
 
+            System.out.println();
+            System.out.println(username+ " : " + user.toString());
+            System.out.println(computername+ " : " + computer.toString());
+
             user.getHand().setCards(new ArrayList<>());
             computer.getHand().setCards(new ArrayList<>());
+
+            numOfGames++;
+
         }
 
-
-//        // create a deck
-//        Deck deck = new Deck();
-//
-//        // deal user
-//        deck.deal(user);
-//        deck.deal(user);
-//        System.out.println(username +"'s hand value is " + user.getHand().toString());
-//        System.out.println("Hand value= "+ user.getHand().returnScore());
-//        System.out.println("would you like another card? answer yes or no");
-//        String anotherCard = scanner.nextLine();
-//        while(anotherCard.compareToIgnoreCase("yes")==0){
-//            deck.deal(user);
-//            System.out.println(user.getHand().toString());
-//            System.out.println("Hand value="+ user.getHand().returnScore());
-//
-//            if(user.getHand().over21()){
-//                System.out.println(username + "'s hand value is over 21");
-//                break;
-//            }
-//
-//            System.out.println("would you like another card? answer yes or no");
-//            anotherCard = scanner.nextLine();
-//        }
-//
-//        // deal computer
-//        System.out.println("\nNow " + computername + "'s turn");
-//        deck.deal(computer);
-//        deck.deal(computer);
-//        System.out.println(computername + " will choose whether to take another card");
-//        while(computer.computerAI()){
-//            deck.deal(computer);
-//            System.out.println(computername + " took one more");
-//
-//            if(computer.getHand().over21()){
-//                System.out.println(computername + "'s hand value is over 21");
-//                break;
-//            }
-//        }
-//
-//        // show the result
-//        System.out.println("\n" + username + "'s hand and hand value are below:");
-//        System.out.println(user.getHand().toString());
-//        System.out.println("Hand value="+ user.getHand().returnScore());
-//
-//        System.out.println("\n" + computername + "'s hand and hand value are below:");
-//        System.out.println(computer.getHand().toString());
-//        System.out.println("Hand value="+ computer.getHand().returnScore());
-//
-//        System.out.println("\nTherefore...");
-//
-//        if(user.getHand().over21() && computer.getHand().over21()) {
-//            System.out.println("Both you lost");
-//        }else if(user.getHand().over21() && !computer.getHand().over21()){
-//            System.out.println(computername + " win");
-//        }else if(!user.getHand().over21() && computer.getHand().over21()){
-//            System.out.println(username + " win");
-//        }else{
-//            if(user.getHand().returnScore() < computer.getHand().returnScore()){
-//                System.out.println(computername + " win");
-//            }else if(user.getHand().returnScore() > computer.getHand().returnScore()){
-//                System.out.println(username + " win");
-//            }else{
-//                System.out.println("No winner, and try again");
-//            }
-//        }
-
+        System.out.println("# of Games = "+numOfGames);
+        System.out.println("# of wins by "+ username + " = "+ numOfWins);
+        System.out.println("# of wins by "+ computername + " = "+ numOfLoss);
 
     }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Player.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Player.java
@@ -16,11 +16,34 @@ public class Player {
         return hand;
     }
 
-    public boolean computerAI(){
-        if (getHand().returnScore()<16){
+    public int getPotValue() {
+        return potValue;
+    }
+
+    public void setPotValue(int potValue) {
+        this.potValue = potValue;
+    }
+
+    public boolean computerAI() {
+        if (getHand().returnScore() < 16) {
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
+
+    public void Betting(int bet, boolean result) {
+        if (result) {
+            this.potValue += bet * 2;
+            System.out.println("Return is " + bet * 2);
+        } else {
+            this.potValue -= bet;
+            System.out.println("Loss is -" + bet);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "current potValue=" + this.potValue;
     }
 }
