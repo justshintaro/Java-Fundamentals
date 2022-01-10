f7b899a4f2ea4fad811bd2ad7ed197b5221a8b02
justshintaro <snakamura01@manhattan.edu>
2022-01-10 10:45:54 -0500

    updated input output some of the exercises

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -9,10 +11,51 @@ package labs_examples.input_output.labs;
  *
  */
 
-class Example {
+class Example01 {
     public static void main(String[] args) {
 
+        String fileReadPath = new  String("C:/Users/shin2/Documents/Shintaro/00_CodingNomads/labs/online-java-fundamentals/src/labs_examples/input_output/files/byte_data");
+        String fileWritePath = new  String("C:/Users/shin2/Documents/Shintaro/00_CodingNomads/labs/online-java-fundamentals/src/labs_examples/input_output/labs/byte_data_copy");
+
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        BufferedInputStream bufferedInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+
+        try {
+            fin = new FileInputStream(fileReadPath);
+            fout = new FileOutputStream(fileWritePath);
+            bufferedInputStream = new BufferedInputStream(fin);
+            bufferedOutputStream = new BufferedOutputStream(fout);
 
 
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1){
+                fout.write(buffer);
+                System.out.print(new String(buffer, 0, bytesRead));
+            }
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        } finally {
+            try {
+                if(fin != null){
+                    fin.close();
+                    bufferedInputStream.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            try {
+                if(fout != null) {
+                    fout.close();
+                    bufferedOutputStream.close();
+                }
+            } catch(IOException exc){
+                System.out.println("Error Closing Output File");
+            }
+        }
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -7,8 +9,69 @@ package labs_examples.input_output.labs;
  *      -For example, change every 'a' to '-' and every 'e' to '~' .
  *      -Make sure you close the connections to both files.
  *
- *      Then, ead back the encrypted file using the BufferedReader and
+ *      Then, read back the encrypted file using the BufferedReader and
  *      print out the unencrypted version. Does it match the original file?
  *
  */
+class Example02 {
+    public static void main(String[] args) {
 
+        int i;
+
+        String fileReadPath = new String("C:/Users/shin2/Documents/Shintaro/00_CodingNomads/labs/online-java-fundamentals/src/labs_examples/input_output/files/char_data.txt");
+        String fileWritePath = new String("C:/Users/shin2/Documents/Shintaro/00_CodingNomads/labs/online-java-fundamentals/src/labs_examples/input_output/labs/char_data_encrypted.txt");
+
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        try {
+            fin = new FileInputStream(fileReadPath);
+            fout = new FileOutputStream(fileWritePath);
+
+            do {
+                i = fin.read();
+                if(i != -1)
+                    if((char) i == 'a')
+                        i = '-';
+                    fout.write(i);
+                    if((char) i == 'e')
+                        i = '~';
+                    fout.write(i);
+                    if((char) i == 'u')
+                        i = '&';
+                    fout.write(i);
+                    if((char) i == 'o')
+                        i = '%';
+                    fout.write(i);
+                    if((char) i == 'i')
+                        i = '$';
+                    fout.write(i);
+            } while(i != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(fin != null){
+                    fin.close();
+                }
+            } catch (IOException e) {
+                System.out.println("I/O Error: " + e);
+            }
+            finally {
+            try {
+                if(fin != null){
+                    fin.close();
+                }
+            }catch(IOException exc) {
+                System.out.println("Error Closing Input File");
+            }
+            try {
+                if(fout != null){
+                    fout.close();
+                }
+            } catch(IOException exc){
+                System.out.println("Error Closing Output FIle");
+            }
+        }
+    }
+}
