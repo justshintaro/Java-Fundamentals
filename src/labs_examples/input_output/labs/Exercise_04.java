34df4bae189e6a1cb39e1eaeb8e8864230c04c35
justshintaro <snakamura01@manhattan.edu>
2022-01-13 13:20:42 -0500

    finalized input and output

--- src/labs_examples/input_output/labs/Exercise_04.java
+++ src/labs_examples/input_output/labs/Exercise_04.java
@@ -1,5 +1,11 @@
 package labs_examples.input_output.labs;
 
+import com.sun.javafx.image.IntPixelGetter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
 /**
  *
  *      Following the video examples and source code found in the src/labs_examples/input_output/examples/csv_parser
@@ -12,3 +18,65 @@ package labs_examples.input_output.labs;
  *      Then, write the arraylist of objects back out to a new .csv file. Ensure that the resulting csv file is valid.
  *
  */
+
+class Exercise04{
+    public static void main(String[] args) {
+
+        ArrayList<FantasyLeader> fantasyLeaders = new ArrayList<>();
+
+        String filePath = "src/labs_examples/input_output/labs/fantasyLeaders.csv";
+
+        try (BufferedReader bufferedReader =
+                     new BufferedReader(new FileReader(filePath))){
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null){
+                String[] values = line.split(",");
+                fantasyLeaders.add(mapValuesToFantasyObject(values));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(FantasyLeader fantasyLeader : fantasyLeaders){
+            System.out.println(fantasyLeader.toString());
+        }
+
+
+        // write back to a newly created csv file
+        System.out.println();
+        String filePathWrite = new  String("src/labs_examples/input_output/labs/fantasyLeaders_copy.csv");
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePathWrite))){
+            for(FantasyLeader fantasyLeader : fantasyLeaders){
+                System.out.println(fantasyLeader);
+                bufferedWriter.write(fantasyLeader.getRank()+","+fantasyLeader.getFirstName()+","+
+                        fantasyLeader.getLastName()+","+fantasyLeader.getTeam()+","+fantasyLeader.getPosition()+","+
+                        fantasyLeader.getPoints()+","+fantasyLeader.getGames()+","+fantasyLeader.getAvg());
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static FantasyLeader mapValuesToFantasyObject(String[] values){
+        FantasyLeader fantasyLeader = new FantasyLeader();
+
+        fantasyLeader.setRank(Integer.parseInt(values[0]));
+        fantasyLeader.setFirstName(values[1]);
+        fantasyLeader.setLastName(values[2]);
+        fantasyLeader.setTeam(values[3]);
+        fantasyLeader.setPosition(values[4]);
+        fantasyLeader.setPoints(Double.parseDouble(values[5]));
+        fantasyLeader.setGames(Integer.parseInt(values[6]));
+        fantasyLeader.setAvg(Double.parseDouble(values[7]));
+
+        return fantasyLeader;
+    }
+}
---
+++ src/labs_examples/input_output/labs/FantasyLeader.java
@@ -0,0 +1,105 @@
+package labs_examples.input_output.labs;
+
+public class FantasyLeader {
+
+    private int rank;
+    private String firstName;
+    private String lastName;
+    private String team;
+    private String position;
+    private double points;
+    private int games;
+    private double avg;
+
+    public FantasyLeader() {
+    }
+
+    public FantasyLeader(int rank, String firstName, String lastName, String team, String position, double points, int games, double avg) {
+        this.rank = rank;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.team = team;
+        this.position = position;
+        this.points = points;
+        this.games = games;
+        this.avg = avg;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    public void setPoints(double points) {
+        this.points = points;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    public void setAvg(double avg) {
+        this.avg = avg;
+    }
+
+    @Override
+    public String toString() {
+        return "FantasyLeader{" +
+                "rank=" + rank +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", team='" + team + '\'' +
+                ", position='" + position + '\'' +
+                ", points=" + points +
+                ", games=" + games +
+                ", avg=" + avg +
+                '}';
+    }
+}
